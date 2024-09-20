dd9166fdcf38fdf7f0390110aa343b7ea940ccfe
Tshering Nidup <ceering.nidup.7@gmail.com>
2024-09-20 11:11:19 +0600

    Changed the styling to auto typing and word

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 var typed = new Typed('.typed', {
-    strings: [' a Front end Developer', ' an ICT & Coding Trainer', ' an Educator.'],
+    strings: [' a Front End Developer', 'an ICT Trainer', ' a Coding Trainer', 'an Educator.'],
     typeSpeed: 80,
     backSpeed: 80,
     loop: true
